18d6a6ea969e7efd0aac448b7d6f3a3ab5d25872
Vio <43034953+limpido@users.noreply.github.com>
2022-07-26 18:46:31 +0800

    Time: 47 ms (59.21%), Space: 13.8 MB (66.35%) - LeetHub

--- 806-number-of-lines-to-write-string/806-number-of-lines-to-write-string.py
+++ 806-number-of-lines-to-write-string/806-number-of-lines-to-write-string.py
@@ -4,10 +4,9 @@ class Solution:
         width = 0
         for ch in s:
             w = widths[ord(ch) - ord('a')]
-            if width + w > 100:
+            width += w
+            if width > 100:
                 lines += 1
                 width = w
-                continue
-            width += w
         return [lines, width]
                 

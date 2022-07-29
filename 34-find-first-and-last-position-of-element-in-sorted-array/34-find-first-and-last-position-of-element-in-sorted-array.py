5f276092885d0f816d95b251bea50e06417cdbbf
Vio <43034953+limpido@users.noreply.github.com>
2022-07-29 17:16:08 +0800

    Time: 147 ms (34.24%), Space: 15.5 MB (50.23%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,18 +2,34 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         if len(nums) == 0:
             return [-1, -1]
+        
+        res = []
+        
+        # binary search left border
         left, right = 0, len(nums)-1
         while left <= right:
             mid = (left+right) // 2
-            if nums[mid] < target:
+            if nums[mid] == target:
+                right = mid-1
+            elif nums[mid] < target:
                 left = mid+1
             elif nums[mid] > target:
                 right = mid-1
-            else:
-                start, end = mid, mid
-                while start >= 0 and nums[start] == target:
-                    start -= 1
-                while end < len(nums) and nums[end] == target:
-                    end += 1
-                return [start+1, end-1]
-        return [-1, -1]
+                
+        if left < len(nums) and nums[left] == target:
+            res.append(left)
+        else: return [-1, -1]
+        
+        # binary search right border
+        right = len(nums)-1
+        while left <= right:
+            mid = (left+right) // 2
+            if nums[mid] == target:
+                left = mid+1
+            elif nums[mid] < target:
+                left = mid+1
+            elif nums[mid] > target:
+                right = mid-1
+        res.append(right)
+        return res
+        

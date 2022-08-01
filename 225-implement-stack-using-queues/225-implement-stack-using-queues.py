3d4bdd95bbecbbdbfdaba3876556423c227df3d3
Vio <43034953+limpido@users.noreply.github.com>
2022-08-01 14:17:51 +0800

    Time: 30 ms (94.46%), Space: 13.9 MB (74.83%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.py
@@ -2,19 +2,22 @@ class MyStack:
 
     def __init__(self):
         self.queue = deque()
+        self.top_el = None
 
     def push(self, x: int) -> None:
         self.queue.append(x)
+        self.top_el = x
 
     def pop(self) -> int:
         for i in range(len(self.queue)-1):
-            self.queue.append(self.queue.popleft())
+            if i == len(self.queue)-2:
+                self.top_el = self.queue.popleft()
+                self.push(self.top_el)
+            else: self.queue.append(self.queue.popleft())
         return self.queue.popleft()
 
     def top(self) -> int:
-        ans = self.pop()
-        self.push(ans)
-        return ans
+        return self.top_el
 
     def empty(self) -> bool:
         return not self.queue

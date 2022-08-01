0cac07fa1b66d83be5dc49bc86066d492c8066f9
Vio <43034953+limpido@users.noreply.github.com>
2022-08-01 14:27:50 +0800

    Time: 185 ms (22.50%), Space: 15 MB (7.41%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -1,9 +1,25 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        stack = []
-        for ch in s:
-            if len(stack) == 0 or stack[-1] != ch:
-                stack.append(ch)
+        '''method 1: stack'''
+        # stack = []
+        # for ch in s:
+        #     if len(stack) == 0 or stack[-1] != ch:
+        #         stack.append(ch)
+        #     else:
+        #         stack.pop()
+        # return "".join(stack)
+        
+        '''method 2: two pointers'''
+        res = list(s)
+        slow = fast = 0
+
+        while fast < len(res):
+            res[slow] = res[fast]
+            
+            if slow > 0 and res[slow] == res[slow - 1]:
+                slow -= 1
             else:
-                stack.pop()
-        return "".join(stack)
+                slow += 1
+            fast += 1
+            
+        return ''.join(res[0: slow])

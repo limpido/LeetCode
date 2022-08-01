3f348516430897565430eb06bdc0fa69115c0dc2
Vio <43034953+limpido@users.noreply.github.com>
2022-08-01 14:03:03 +0800

    Time: 52 ms (35.74%), Space: 14 MB (75.77%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.py
@@ -5,18 +5,18 @@ class MyQueue:
         self.stack2 = []
 
     def push(self, x: int) -> None:
-        while self.stack2:
-            self.stack1.append(self.stack2.pop())
         self.stack1.append(x)
 
     def pop(self) -> int:
-        while self.stack1:
-            self.stack2.append(self.stack1.pop())
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
         return self.stack2.pop()
 
     def peek(self) -> int:
-        while self.stack1:
-            self.stack2.append(self.stack1.pop())
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
         return self.stack2[-1]
 
     def empty(self) -> bool:

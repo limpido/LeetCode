9f700d006ac475d7aaebfba6e40d43a7090e8a02
Vio <43034953+limpido@users.noreply.github.com>
2022-08-02 16:04:28 +0800

    Time: 39 ms (81.05%), Space: 13.8 MB (53.92%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,7 +1,20 @@
 class Solution:
     def fib(self, n: int) -> int:
+        '''method 1: recursion'''
+        # if n <= 1: return n
+        # return self.fib(n-1) + self.fib(n-2)
+    
+        '''method 2: dp'''
+        # if n <= 1: return n
+        # dp = [0, 1]
+        # for i in range(2, n+1):
+        #     dp.append(dp[i-1] + dp[i-2])
+        # return dp[n]
+        
+        '''method 3: dp space O(1)'''
         if n <= 1: return n
-        dp = [0, 1]
+        fib1, fib2 = 0, 1
         for i in range(2, n+1):
-            dp.append(dp[i-1] + dp[i-2])
-        return dp[n]
+            fib3 = fib1+fib2
+            fib1, fib2 = fib2, fib3
+        return fib3

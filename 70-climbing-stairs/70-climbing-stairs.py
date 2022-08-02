f9f22b33127773ffd04e7e58df90d674d065da46
Vio <43034953+limpido@users.noreply.github.com>
2022-08-02 16:14:05 +0800

    Time: 38 ms (75.13%), Space: 13.8 MB (57.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,9 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         '''dp[n] = dp[n-1] + dp[n-2]'''
-        dp = [1, 2]
         if n <= 2:
-            return dp[n-1]
+            return n
+        dp = [0] * n
+        dp[0], dp[1] = 1, 2
         for i in range(2, n):
-            dp.append(dp[i-1] + dp[i-2])
-        return dp[-1]
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n-1]

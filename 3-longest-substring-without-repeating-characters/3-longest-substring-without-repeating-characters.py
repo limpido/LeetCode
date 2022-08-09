5ae4523ea271c330e6dd2fa1b6ea28a547c92a0e
Vio <43034953+limpido@users.noreply.github.com>
2022-08-09 14:41:28 +0800

    Time: 82 ms (75.65%), Space: 13.9 MB (93.16%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,5 +1,6 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        '''
         seen = set()
         
         left, right = 0, 0
@@ -13,5 +14,24 @@ class Solution:
             seen.add(s[right])
             max_length = max(max_length, right-left+1)
             right += 1
+        return max_length
+        '''
+        
+        seen = {}
+        left, right = 0, 0
+        max_length = 0
+        
+        while right < len(s):
+            if s[right] in seen:
+                if seen[s[right]] < left:
+                    max_length = max(max_length, right-left+1)
+                else:
+                    left = seen[s[right]] + 1
+            else:
+                max_length = max(max_length, right-left+1)
+            
+            seen[s[right]] = right
+            right += 1
+            
         return max_length
             

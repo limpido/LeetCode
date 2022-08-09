a64a22a658b0eeb0a1389c6618a70d6ef8a3e4e0
Vio <43034953+limpido@users.noreply.github.com>
2022-08-09 15:31:15 +0800

    Time: 1131 ms (75.23%), Space: 13.9 MB (91.35%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,9 +1,9 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        '''
+        ''' Method 1: DP
         dp[i, j]: whether the substring between the index i and j is palindrome
         dp[i, j] = dp[i+1, j-1] and s[i] == s[j]
-        '''
+
         
         if len(s) == 1: return s
         if len(s) == 2:
@@ -22,3 +22,22 @@ class Solution:
                     max_length = j-i+1
                     res = s[i:j+1]
         return res
+        '''
+        
+        '''Method 2: Expand Around Center'''
+        def expandAroundCenter(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return r-l-1   # r-1 - (l+1) + 1 = r-l-1
+        
+        if len(s) == 1: return s
+        l, r = 0, 0
+        for i in range(len(s)):
+            odd = expandAroundCenter(i, i)
+            even = expandAroundCenter(i, i+1)
+            length = max(odd, even)
+            if length > r-l+1:
+                l = i- (length-1) // 2
+                r = i+ length // 2
+        return s[l:r+1]

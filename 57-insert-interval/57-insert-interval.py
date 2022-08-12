930f6e515553442a4d984d9cd44449d3a9d2c69c
Vio <43034953+limpido@users.noreply.github.com>
2022-08-12 13:22:21 +0800

    Time: 153 ms (24.12%), Space: 17.3 MB (53.11%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,5 +1,6 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        ''' Method 1: same as merge interval, O(nlogn)
         intervals.append(newInterval)
         intervals.sort(key=lambda x: x[0])
         merged = []
@@ -10,3 +11,18 @@ class Solution:
                 merged[-1][0] = min(merged[-1][0], interval[0])
                 merged[-1][1] = max(merged[-1][1], interval[1])
         return merged
+        '''
+        
+        '''Method 2: O(n)'''
+        res = []
+        for interval in intervals:
+            if interval[1] < newInterval[0]:
+                res.append(interval)
+            elif interval[0] > newInterval[1]:
+                res.append(newInterval)
+                newInterval = interval
+            else:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(interval[1], newInterval[1])
+        res.append(newInterval)
+        return res

a3080824b575fe969aaf19fa4fc60f0a7101117e
Vio <43034953+limpido@users.noreply.github.com>
2022-08-16 14:17:14 +0800

    Time: 68 ms (74.78%), Space: 14.2 MB (15.38%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        '''Method 1: O(n)'''
+        '''Method 1: O(n)
         miss = []
         i = 0
         n = 1
@@ -12,3 +12,31 @@ class Solution:
             n += 1
             
         return miss[k-1] if len(miss) >= k else arr[-1]+ k-len(miss)
+        '''
+        
+        '''Method 2: Set
+        arr_set = set(arr)
+        for i in range(1, k + len(arr) + 1):
+            if i not in arr_set: k -= 1
+            if k == 0: return i
+        '''
+        
+        '''Method 3: O(logn) binary search
+        amount of missing numbers before arr[i]: arr[i]-i-1
+        '''
+        l, r = 0, len(arr)-1
+        while l < r:
+            mid = (l+r) // 2
+            missing_num = arr[mid]-mid-1
+            if missing_num < k:
+                l = mid+1
+            else:
+                r = mid-1
+        
+        missing_num = arr[l]-l-1
+        if missing_num >= k:
+            k = missing_num - k + 1
+            return arr[l] - k
+        else:
+            k = k - missing_num
+            return arr[l]+k

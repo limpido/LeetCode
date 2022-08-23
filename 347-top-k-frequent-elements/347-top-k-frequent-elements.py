e1b2a101265e2a6a336284767a718cbe86d31e5e
Vio <43034953+limpido@users.noreply.github.com>
2022-08-23 21:48:09 +0800

    Time: 255 ms (6.76%), Space: 19.4 MB (29.95%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,6 +1,8 @@
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ''' Method 1: min heap
+        
         map_ = {}  # frequency of nums
         for i in range(len(nums)):
             map_[nums[i]] = map_.get(nums[i], 0) + 1
@@ -16,3 +18,21 @@ class Solution:
         for i in range(k-1, -1, -1):
             result[i] = heapq.heappop(pri_que)[1]
         return result
+        '''
+        
+        ''' Method 2: bucket sort'''
+        hm = {}
+        for i in range(len(nums)):
+            hm[nums[i]] = hm.get(nums[i], 0) + 1
+        
+        bucket = [[] for _ in range(len(nums)+1)]
+        
+        for key in hm.keys():
+            bucket[hm[key]].append(key)
+        
+        res = []
+        for i in range(len(bucket)-1, -1, -1):
+            if len(bucket[i]):
+                res.extend(bucket[i])
+            if len(res) == k:
+                return res

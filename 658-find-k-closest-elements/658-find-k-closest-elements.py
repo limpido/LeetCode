d08f730da2df9ecaf788856f70d9cb6809a58463
Vio <43034953+limpido@users.noreply.github.com>
2022-08-26 12:23:19 +0800

    Time: 309 ms (93.42%), Space: 15.6 MB (45.27%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.py
+++ 658-find-k-closest-elements/658-find-k-closest-elements.py
@@ -1,14 +1,6 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         # sorted, may have duplicates, x may not exist in the array
-        def right_dist(idx):
-            return arr[idx]-x
-        
-        def left_dist(idx):
-            return x-arr[idx]
-        
-        res = []
-        
         idx = 0
         while idx < len(arr):
             if x <= arr[idx]:
@@ -18,23 +10,12 @@ class Solution:
         idx = min(len(arr)-1, idx)
         
         l, r = idx-1, idx
-        while k > 0 and r < len(arr) and l >= 0:
-            rd = right_dist(r)
-            ld = left_dist(l)
-            if rd < ld:
-                res.append(arr[r])
-                r += 1
-            else:
-                res.insert(0, arr[l])
+        while k > 0:
+            if r >= len(arr) or l >= 0 and x-arr[l] <= arr[r]-x:
                 l -= 1
-            k -= 1
-        while k > 0 and r < len(arr):
-            res.append(arr[r])
-            r += 1
-            k -= 1
-        while k > 0 and l >= 0:
-            res.insert(0, arr[l])
-            l -= 1
+            else:
+                r += 1
+            
             k -= 1
         
-        return res
+        return arr[l+1:r]

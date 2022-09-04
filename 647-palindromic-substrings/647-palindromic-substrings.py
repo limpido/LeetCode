11fb7579b95194694710b0f11586dc314f833f94
Vio <43034953+limpido@users.noreply.github.com>
2022-09-04 11:13:27 +0800

    Time: 354 ms (27.86%), Space: 22 MB (20.51%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -4,7 +4,7 @@ class Solution:
         dp[i, j]: number of palindromic substrings
         dp[i, j] = dp[i+1, j] + dp[i, j-1] - dp[i+1][j-1] + (1 if isPalindrome[i][j] else 0)
         '''
-        
+        '''
         palindrome = [[0]*len(s) for _ in range(len(s))]
         dp = [[0]*len(s) for _ in range(len(s))]
         
@@ -16,3 +16,19 @@ class Solution:
                 dp[i][j] = dp[i+1][j] + dp[i][j-1] - dp[i+1][j-1] + (1 if palindrome[i][j] else 0)
                 
         return dp[0][len(s)-1]
+        '''
+        
+    
+        '''
+        count palindromic substrings
+        dp[i, j]: whether substring[i, j] is palindrome
+        '''
+        dp = [[0]*len(s) for _ in range(len(s))]
+        res = 0
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i, len(s)):
+                dp[i][j] = s[i] == s[j] if j <= i+1 else (dp[i+1][j-1] and s[i] == s[j])
+                if dp[i][j]:
+                    res += 1
+                
+        return res

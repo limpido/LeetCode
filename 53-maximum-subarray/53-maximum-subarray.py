96062254db42ce36c91cf20281d57c35106a8a94
Vio <43034953+limpido@users.noreply.github.com>
2022-09-14 17:37:30 +0800

    Time: 1213 ms (43.43%), Space: 28.5 MB (16.83%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,14 +4,24 @@ class Solution:
         dp[i]: max sum of subarray until the index i
         dp[i] = dp[i-1] > 0 ? dp[i-1]+nums[i] : nums[i]
         '''
-        dp = [nums[0]]
-        max_total = nums[0]
+        # dp = [nums[0]]
+        # max_total = nums[0]
+        # for i in range(1, len(nums)):
+        #     if dp[i-1] > 0:
+        #         total = dp[i-1]+nums[i]
+        #         dp.append(total)
+        #         max_total = max(max_total, total)
+        #     else:
+        #         dp.append(nums[i])
+        #         max_total = max(max_total, nums[i])
+        # return max_total
+    
+    
+        max_total, total = nums[0], nums[0]
         for i in range(1, len(nums)):
-            if dp[i-1] > 0:
-                total = dp[i-1]+nums[i]
-                dp.append(total)
-                max_total = max(max_total, total)
+            if total > 0:
+                total += nums[i]
             else:
-                dp.append(nums[i])
-                max_total = max(max_total, nums[i])
+                total = nums[i]
+            max_total = max(max_total, total)
         return max_total

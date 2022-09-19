61e2d46f20fe1f6bfa4c10587e745eaaa5c286c7
Vio <43034953+limpido@users.noreply.github.com>
2022-09-20 00:18:39 +0800

    Time: 66 ms (24.71%), Space: 13.8 MB (97.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,9 +4,28 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        '''method 1: bubble sort'''
-        for j in range(len(nums)):
-            for i in range(len(nums)-1):
-                if nums[i] > nums[i+1]:
-                    nums[i], nums[i+1] = nums[i+1], nums[i]
-            # print(nums)
+        '''method 1: bubble sort, O(n^2)'''
+        # for j in range(len(nums)):
+        #     for i in range(len(nums)-1):
+        #         if nums[i] > nums[i+1]:
+        #             nums[i], nums[i+1] = nums[i+1], nums[i]
+            
+        '''method 2: count sort'''
+        count = [0]*3
+        for n in nums:
+            count[n] += 1
+        
+        p = 0
+        for i in range(len(nums)):
+            while p < len(count) and count[p] == 0:
+                p += 1
+            if p == len(count):
+                break
+            
+            nums[i] = p
+            count[p] -= 1
+            
+        
+        
+        
+        

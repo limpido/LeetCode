c3b9c51b793bdbe0906922419224f51d0dd37c1e
Vio <43034953+limpido@users.noreply.github.com>
2022-09-20 00:09:29 +0800

    Time: 94 ms (5.12%), Space: 13.9 MB (64.52%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        '''method 1: bubble sort'''
+        for j in range(len(nums)):
+            for i in range(len(nums)-1):
+                if nums[i] > nums[i+1]:
+                    nums[i], nums[i+1] = nums[i+1], nums[i]
+            # print(nums)

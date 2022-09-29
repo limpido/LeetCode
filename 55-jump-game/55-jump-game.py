7e17caab626a607a3c1c0b8ef69621527d4188fd
Vio <43034953+limpido@users.noreply.github.com>
2022-09-29 11:34:48 +0800

    Time: 457 ms (99.42%), Space: 15.3 MB (18.32%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -3,15 +3,20 @@ class Solution:
         '''
         method 1
         '''
+        last_pos = len(nums)-1
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] >= last_pos-i:
+                last_pos = i
+        return last_pos == 0
         
         '''
-        method 2: max position so far
+        method 2: max position so far, O(n)
         '''
-        max_pos = 0
-        i = 0
-        while i <= max_pos:
-            max_pos = max(max_pos, nums[i]+i)
-            if max_pos >= len(nums)-1:
-                return True
-            i += 1
-        return False
+        # max_pos = 0
+        # i = 0
+        # while i <= max_pos:
+        #     max_pos = max(max_pos, nums[i]+i)
+        #     if max_pos >= len(nums)-1:
+        #         return True
+        #     i += 1
+        # return False
